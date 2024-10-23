6ae0a545af7d6098c4b9f9342281393cd11465ff
EunJin Kim <eunjin9639@gmail.com>
2024-10-24 00:17:44 +0900

    🌈 style: Part I - Background Information

--- src/pages/report/Report.vue
+++ src/pages/report/Report.vue
@@ -16,6 +16,8 @@ const MONTHS = [
     'December',
 ]
 
+const OPTIONS = [{ label: 'Yes', value: true }, { label: 'No', value: false }]
+
 export default {
     components: {
         DownloadIcon
@@ -30,7 +32,7 @@ export default {
             {
                 title: 'Amended Return?',
                 type: 'radio',
-                options: [{ label: 'Yes', value: true }, { label: 'No', value: false }]
+                options: OPTIONS
             },
             {
                 title: 'Number of Sheet/s Attached',
@@ -38,11 +40,19 @@ export default {
             },
         ]
         return {
+            OPTIONS,
             formSelectValue: {
                 month: '',
                 typeSelect: '',
                 amendedReturn: false,
-                numberOfSheetAttached: ''
+                numberOfSheetAttached: '',
+                tin: '',
+                rdoCode: '',
+                agentName: '',
+                address: '',
+                zipCode: '',
+                categoryOfWithholdingAgent: 'private',
+                ifPrivate: ''
             },
             openedSelects: {
                 typeSelect: false,
@@ -147,13 +157,13 @@ export default {
                     </button>
                 </div>
                 <div class="border rounded border-gray-07">
-                    <ul class="grid grid-cols-3 px-3">
+                    <ul class="grid grid-cols-3">
                         <li
                           v-for="(survey, idx) in taxReturnSurvey"
                           :key="idx"
-                          class="inline-flex flex-col items-center gap-4 py-3 border-r last-of-type:border-r-0"
+                          class="inline-flex flex-col items-center gap-4 px-3 py-3 border-r last-of-type:border-r-0"
                         >
-                            <div class="inline-flex items-center">
+                            <div class="inline-flex items-center w-full">
                                 <strong>{{ idx + 1 }}. {{ survey.title }} </strong>
                                 <button
                                   v-if="survey.type === 'select'"
@@ -162,11 +172,11 @@ export default {
                             </div>
                             <fieldset
                               v-if="survey.type === 'select'"
-                              class="inline-flex flex-col gap-3"
+                              class="inline-flex flex-col w-full gap-3"
                             >
                                 <label>
                                     <div
-                                      class="font-bold w-60 select"
+                                      class="w-full font-bold select"
                                       ref="month"
                                       :class="[!formSelectValue.month ? 'text-gray-08' : 'text-[#151515]', openedSelects.month && 'rounded-b-none']"
                                       @click="() => toggleSelect('month')"
@@ -193,14 +203,14 @@ export default {
                                     </Teleport>
                                 </label>
                                 <input
-                                  class="w-60 input-field"
+                                  class="w-full input-field"
                                   type="text"
                                 />
                             </fieldset>
 
                             <fieldset
                               v-if="survey.type === 'radio'"
-                              class="inline-flex items-center gap-6"
+                              class="inline-flex items-center w-full gap-6 mt-3"
                             >
                                 <label
                                   v-for="check in survey.options"
@@ -217,13 +227,118 @@ export default {
                                 </label>
                             </fieldset>
 
-                            <fieldset v-if="survey.type === 'input'">
-                                <input class="w-60 input-field" />
+                            <fieldset
+                              v-if="survey.type === 'input'"
+                              class="inline-flex items-center w-full gap-6 mt-3"
+                            >
+                                <input
+                                  class="w-full input-field"
+                                  v-model="formSelectValue.numberOfSheetAttached"
+                                />
                             </fieldset>
                         </li>
                     </ul>
                     <h3 class="title">
                         Part I - Background Information
+                    </h3>
+                    <ul class="px-3">
+                        <li class="inline-flex items-center flex-1 w-full gap-4 py-3">
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">4. Taxpayer Identification Number (TIN)</strong>
+                            </div>
+                            <fieldset class="w-full">
+                                <input
+                                  class="w-full input-field"
+                                  v-model="formSelectValue.tin"
+                                />
+                            </fieldset>
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">5. RDO Code</strong>
+                            </div>
+                            <fieldset class="w-28">
+                                <input
+                                  class="w-28 input-field"
+                                  v-model="formSelectValue.rdoCode"
+                                />
+                            </fieldset>
+                        </li>
+                        <li class="inline-flex flex-col flex-1 w-full gap-4 py-3">
+                            <div class="inline-flex items-center">
+                                <strong>6. Withholding Agent’s Name (Last Name, First Name, Middle Name for Individual
+                                    OR
+                                    Registered Name for Non-Individual)</strong>
+                            </div>
+                            <fieldset class="w-full">
+                                <input
+                                  class="w-full input-field"
+                                  v-model="formSelectValue.agentName"
+                                />
+                            </fieldset>
+                        </li>
+                        <li class="inline-flex items-center flex-1 w-full gap-4 py-3">
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">7. Registered Address</strong>
+                            </div>
+                            <fieldset class="w-full">
+                                <input
+                                  class="w-full input-field"
+                                  v-model="formSelectValue.address"
+                                />
+                            </fieldset>
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">7-A. ZIP Code</strong>
+                            </div>
+                            <fieldset class="w-28">
+                                <input
+                                  class="w-28 input-field"
+                                  v-model="formSelectValue.zipCode"
+                                />
+                            </fieldset>
+                        </li>
+                        <li class="inline-flex items-center flex-1 w-full gap-4 py-3">
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">8. Category of Withholding Agent </strong>
+                            </div>
+                            <fieldset class="inline-flex items-center w-full">
+                                <label
+                                  v-for="check in [{ label: 'Private', value: 'private' }, { label: 'Government', value: 'government' }]"
+                                  :key="check.label"
+                                  class="inline-flex items-center mr-4 cursor-pointer flex-nowrap last-of-type:mr-0"
+                                >
+                                    <input
+                                      type="radio"
+                                      class="mr-2 radio"
+                                      :value="check.value"
+                                      v-model="formSelectValue.categoryOfWithholdingAgent"
+                                    >
+                                    {{ check.label }}
+                                </label>
+                            </fieldset>
+                            <div class="inline-flex items-center">
+                                <strong class="whitespace-nowrap">8-A. If private, top withholding agent?</strong>
+                            </div>
+                            <fieldset class="inline-flex items-center text-nowrap">
+                                <label
+                                  v-for="check in OPTIONS"
+                                  :key="check.label"
+                                  class="inline-flex items-center mr-4 cursor-pointer last-of-type:mr-0"
+                                >
+                                    <input
+                                      type="radio"
+                                      class="mr-2 radio"
+                                      :value="check.value"
+                                      v-model="formSelectValue.ifPrivate"
+                                    >
+                                    {{ check.label }}
+                                </label>
+                            </fieldset>
+                        </li>
+                    </ul>
+                    <h3 class="title">
+                        Part II - Summary of Remittances per BIR Form No. 1601-C
+                    </h3>
+                    <h3 class="title">
+                        Continuation of Part II
                     </h3>
                 </div>
             </form>
